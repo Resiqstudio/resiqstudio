35c52344d3135dbf594ec268243c73f4b4a8fd9a
resiq <darshankusanal1008@gmail.com>
2026-06-21 22:31:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-alert("ResIQ script loaded");
-
-
 function generateResearch(){
-
-alert("Generate button clicked");
-
 
 let topic = document.getElementById("topic").value;
 
+if(topic==""){
+alert("Enter topic");
+return;
+}
 
 document.getElementById("output").innerHTML =
-"Your topic is: " + topic;
+`
+<h2>Your Research Topic</h2>
 
+<p>${topic}</p>
+
+<h3>Abstract</h3>
+<p>This is a sample research abstract about ${topic}.</p>
+
+<h3>Introduction</h3>
+<p>${topic} is an important area of study.</p>
+`;
 
 }
